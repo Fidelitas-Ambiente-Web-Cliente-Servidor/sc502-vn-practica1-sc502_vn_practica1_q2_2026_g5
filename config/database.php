f08deafd6c653745645f892825a7022327f29a6c
PabloHernandez778 <pablogihub08@gmail.com>
2026-07-16 18:43:54 -0600

    Integrando avances a rol1 database.php

---
+++ config/database.php
@@ -0,0 +1,27 @@
+<?php
+
+class Database{
+
+    private static $conexion = null;
+
+    public static function conectar(){
+
+        if(self::$conexion == null){
+
+            $server = "db";
+            $database = "Tarea1_db";
+            $userDB = "root";
+            $passDB = "root";
+
+            self::$conexion = new PDO(
+                "mysql:host=$server;dbname=$database;charset=utf8mb4",
+                $userDB,
+                $passDB
+            );
+
+            self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return self::$conexion;
+    }
+}
